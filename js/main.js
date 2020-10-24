747603467aa47fb404f2f868ec5fa11fd4a2f654
yarishred <yfruiz9@gmail.com>
2020-10-24 12:38:07 -0500

    pruebas

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Variables
+const overlays = {
+  overlay1: document.querySelector(".overlay1"),
+  overlay2: document.querySelector(".overlay2"),
+  overlayBar: document.querySelector(".overlay-bar"),
+  logo: document.querySelector(".enroladosLogo"),
+  menuLink: document.querySelectorAll('.nav-link'),
+  imageCarousel: document.querySelector('.image-carousel')
+};
+
+// Destructuring
+const { 
+  overlay1, 
+  overlay2, 
+  overlayBar, 
+  logo,
+  menuLink,
+  imageCarousel  
+} = overlays;
+
+// Events
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    overlay1.style.width = "50%";
+    overlay2.style.width = "50%";
+  }, 2000);
+  setTimeout(() => {
+    overlayBar.style.height = "100vh";
+  }, 2000);
+  setTimeout(() => {
+    overlayBar.style.opacity = "0";
+    overlay2.style.background = "#eebd06";
+  }, 3500);
+  setTimeout(() => {
+    logo.classList.add("logoTransition");
+    menuLink.forEach(menu => menu.style.opacity = '1')
+  }, 5000);
+  setTimeout(() => {
+    imageCarousel.style.opacity = '1'
+  }, 5800);
+});
